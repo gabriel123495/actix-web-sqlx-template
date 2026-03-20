6453b879cce2c46d3427889d68794d6735c3a2d6
gabriel <gabriel.leonardo.cruz@outlook.com>
2026-03-20 12:46:44 -0300

    atualizando erros

--- src/models.rs
+++ src/models.rs
@@ -6,24 +6,26 @@ pub struct User {
     pub id: i32,
     pub name: String,
     pub email: String,
+    pub password: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CreateUserRequest {
     pub name: String,
     pub email: String,
+    pub password: String
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UpdateUserRequest {
     pub name: Option<String>,
     pub email: Option<String>,
+    pub password: Option<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct LoginRequest {
     pub name: String,
     pub email: String,
+    pub password: String,
 }
-
-//
